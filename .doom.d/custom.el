48cbd6b9c912b12704e870a1880c30dca82ed1d3
fLock2 <trevorsilbusiness@gmail.com>
2021-10-07 20:39:30 -0400

    update flyspell config

---
+++ .doom.d/custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2E3440" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0" "#ECEFF4"])
+ '(custom-safe-themes
+   '("b732f05b0391fcb3b50e8b79d78c6b903fac7cc894d528eb06408d9bc6da5e67" "e8bbe184ba98a77000a75b261993c3aa4d24e0d1224a96784315b0dae1d6f64a" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default))
+ '(exwm-floating-border-color "#1c2028")
+ '(fci-rule-color "#4C566A")
+ '(highlight-tail-colors ((("#394147") . 0) (("#37424e") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#191C25" "#81A1C1"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#191C25" "#A3BE8C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#191C25" "#434C5E"))
+ '(objed-cursor-color "#BF616A")
+ '(package-selected-packages
+   '(flycheck org-bullets solarized-theme ac-ispell org-preview-html markdown-mode vmd-mode nord-theme ##))
+ '(pdf-view-midnight-colors (cons "#ECEFF4" "#2E3440"))
+ '(rustic-ansi-faces
+   ["#2E3440" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0" "#ECEFF4"])
+ '(vc-annotate-background "#2E3440")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A3BE8C")
+    (cons 40 "#bbc28b")
+    (cons 60 "#d3c68b")
+    (cons 80 "#EBCB8B")
+    (cons 100 "#e2b482")
+    (cons 120 "#d99d79")
+    (cons 140 "#D08770")
+    (cons 160 "#c68984")
+    (cons 180 "#bd8b98")
+    (cons 200 "#B48EAD")
+    (cons 220 "#b77f96")
+    (cons 240 "#bb7080")
+    (cons 260 "#BF616A")
+    (cons 280 "#a05b67")
+    (cons 300 "#815664")
+    (cons 320 "#625161")
+    (cons 340 "#4C566A")
+    (cons 360 "#4C566A")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
