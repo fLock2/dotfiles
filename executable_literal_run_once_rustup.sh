3882f4956efba304e6ba6bda36e6ab3ba2a98a32
fLock S <flock@traumbox.lan>
2023-12-24 18:37:37 -0500

    Modify rustup

--- executable_literal_run_once_rustup.sh
+++ executable_literal_run_once_rustup.sh
@@ -9,5 +9,5 @@ source ~/.profile
 # Install cargo binstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
-# Install bob-nvim
-cargo binstall bob-nvim
+# Install bacon
+cargo install --locked bacon
