3af59092592e4c439caee3eef85b7b9573cb7ea6
fLock S <flock@traumbox.lan>
2024-01-02 13:12:10 -0500

    Switch to LazyVim and nushell

---
+++ dot_config/nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      variant = "main",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
